926feaa62bf17b8d92be3f8a74da9f26e9c2ec07
Stur Shi <sdongjie@126.com>
2016-06-21 14:09:51 +0800

    auto commit

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post("/ubt/pv", routes.ubt.pv);
   For H5
 */
 app.get("/ubt/pv.gif", routes.ubt.pvgif);
-app.get("/ubt/error", function(req,res){res.sendStatus(200)});
+app.get("/ubt/error.gif", function(req,res){res.sendStatus(200)});
 app.get("/ubt/q", routes.ubt.q);
 
 routes.mapreduce = require('./route/mr.js');
