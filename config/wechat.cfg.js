05eb605ccaee326286b890c09d713e530b133fd9
Stur Shi <sdongjie@126.com>
2016-06-29 12:14:15 +0800

    auto commit

--- config/wechat.cfg.js
+++ config/wechat.cfg.js
@@ -2,8 +2,13 @@ module.exports = {
     grant_type: 'client_credential',
     //appid: 'wx903bcebe9afcdbc1',
     //secret: '46c459184a9c545773e5bf1ad6ee3a7a',
-    appid: 'wx1063580e84d2c111',
-    secret: '6840cf0089c141b3c578b86954a522fc',
+
+    //appid: 'wx1063580e84d2c111',
+    //secret: '6840cf0089c141b3c578b86954a522fc',
+
+    appid: 'wx1b3ada37970d5fb3',
+    secret: '9f2edc0e4502136ad28d632664fb9a24',
+
     noncestr:'Wm3WZYTPz0wzccnW',
     accessTokenUrl:'https://api.weixin.qq.com/cgi-bin/token',
     ticketUrl:'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
