248ff7bdb9e51ef050545e01bc2a9d3aa3fdfd6f
Stur Shi <dongjie.shi@ichezheng.com>
2016-11-24 11:40:08 +0800

    auto commit

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ log4js.configure({
 var logger = log4js.getLogger('normal');
 
 var app = express();
-app.use(bodyParser.json({limit: '20mb'}));
-app.use(bodyParser.urlencoded({ extended: true,limit: '20mb', parameterLimit:50 }));
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({ extended: true,limit: '10mb', parameterLimit:50 }));
 
 if (config.env=='pro') {
   logger.setLevel('WARN');
