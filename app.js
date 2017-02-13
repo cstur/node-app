d6bab0d8157d1da9248d65a6c9785564aa57fae0
ShiDongjie <dongjie.shi@baidao.com>
2017-02-13 17:37:53 +0800

    change to express server

--- app.js
+++ app.js
@@ -154,19 +154,19 @@ process.on('uncaughtException', function(err) {
     logger.error(err);
 });
 
-/*
+
 app.listen(port, function() {
 	logger.info("Listening on " + port);
 });
-*/
+
 //var privateKey  = fs.readFileSync('sslcert/ssl.key', 'utf8');
 //var certificate = fs.readFileSync('sslcert/ssl.crt', 'utf8');
 
 //var credentials = {key: privateKey, cert: certificate};
-var httpServer = http.createServer(app);
+//var httpServer = http.createServer(app);
 //var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(port);
+//httpServer.listen(port);
 //httpsServer.listen(8443,"0.0.0.0");
 
 var WebSocketServer = require('ws').Server
